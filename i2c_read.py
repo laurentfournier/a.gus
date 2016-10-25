4815ea196349c3ee06160af03b7348ff30bca228
OkTekk <mail@laurent-fournier.be>
2016-10-25 12:40:50 +0200

    Removed unused function (write data to file)

--- i2c_read.py
+++ i2c_read.py
@@ -24,11 +24,6 @@ bus = i2c_helper.get_smbus()
 adc = ADCPi(bus, 0x68, 0x69, 18)
 
 
-def writetofile(texttowrtite):
-    f = open('adclog.txt', 'a')
-    f.write(str(datetime.datetime.now()) + " " + texttowrtite)
-    f.closed
-
 while True:
     # read from 8 adc channels and print it to terminal
     print("%02f %02f %02f %02f %02f %02f %02f %02f\n" % (adc.read_voltage(1), adc.read_voltage(2), adc.read_voltage(3), adc.read_voltage(4), adc.read_voltage(5), adc.read_voltage(6), adc.read_voltage(7), adc.read_voltage(8)))
