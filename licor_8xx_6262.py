012b8ca469da5aca5c2b22d461e986affcb679c8
Laurent Fournier <subskanz@gmail.com>
2016-11-04 22:59:52 +0100

    Added devices switch

--- licor_8xx_6262.py
+++ licor_8xx_6262.py
@@ -42,6 +42,50 @@ BAUD       = 9600
 PARITY     = 'N'
 STOPBIT    = 1
 BYTE_SZ    = 8
+'''
+    IO data from/to a Licor 820, 840 and 6262
+    
+    Written by David H. Hagan, May 2016
+    Modifications by Laurent Fournier, October 2016
+
+    To-do :
+    - Devices switch
+    - For 820:  celltemp, cellpres, co2
+    - For 840:  celltemp, cellpres, co2, h2o, h2odewpoint
+    - For 6262: celltemp, cellpres, co2, h2o, h2odewpoint
+'''
+
+import os, sys, subprocess
+import time, datetime
+import serial
+import argparse
+#import string
+
+from bs4  import BeautifulSoup as bs
+from lxml import etree
+
+
+#-------------------------------------------------------------
+#------------------ Open configurations ----------------------
+#-------------------------------------------------------------
+
+  ############
+  # Settings #
+  ############
+
+CONFIG     = args.config
+CONTINUOUS = args.continuous
+DEBUG      = args.debug
+LOG        = args.logging
+LOOPS      = args.loops                                                                 # Nr of data extractions
+DEVICE     = args.model                                                                 # List of devices's models
+
+FREQ       = 60
+PORT       = '/dev/ttyUSB0'
+BAUD       = 9600
+PARITY     = 'N'
+STOPBIT    = 1
+BYTE_SZ    = 8
 TIMEOUT    = 5.0
 LOG_DIR    = 'logs/'
 
@@ -61,26 +105,50 @@ class Licor:
         self.timeout    = kwargs.pop('timeout', TIMEOUT)
         self.debug      = kwargs.pop('debug',   DEBUG)
 
-        if CONFIG:                                                                      # Write to the device
-            self._header = [ 'li820',                                                   # (RW)Must be present all the time
-                             'rs232',                                                   # (R)
-                             'celltemp',                                                # 
-                             'cellpres',                                                # 
-                             'co2',                                                     # 
-                             'co2abs',                                                  # 
-                             'ivolt',                                                   # 
-                             'raw',                                                     # 
-                             'data',                                                    # (W)
-                             'cfg',                                                     # (W)
-                             'cal',                                                     # (W)
-                             'outrate']                                                 # (W)
-            
-        else:                                                                           # Read from the device
-            self._header = [ 'date',
-                             'time',
-                             'cell_temp',
-                             'cell_pressure',
-                             'co2']
+        if DEVICE is 820:
+            if CONFIG:                                                                  # Write to the device
+                #self._header = [ line.strip() for line in open(todo, 'r')]
+                self._header = [ 'li820',                                               # (RW)Must be present all the time
+                                 'rs232',                                               # (R)
+                                 'celltemp',                                            # 
+                                 'cellpres',                                            # 
+                                 'co2',                                                 # 
+                                 'co2abs',                                              # 
+                                 'ivolt',                                               # 
+                                 'raw',                                                 # 
+                                 'data',                                                # (W)
+                                 'cfg',                                                 # (W)
+                                 'cal',                                                 # (W)
+                                 'outrate']                                             # (W)
+                
+            else:                                                                       # Read from the device
+                self._header = [ 'date',
+                                 'time',
+                                 'cell_temp',
+                                 'cell_pressure',
+                                 'co2']
+                
+        elif DEVICE is 840:
+            if CONFIG:
+                self._header = [ 'li820',                                               # (RW)Must be present all the time
+                                 'rs232',                                               # (R)
+                                 'celltemp',                                            # 
+                                 'cellpres',                                            # 
+                                 'co2',                                                 # 
+                                 'co2abs',                                              # 
+                                 'ivolt',                                               # 
+                                 'raw',                                                 # 
+                                 'data',                                                # (W)
+                                 'cfg',                                                 # (W)
+                                 'cal',                                                 # (W)
+                                 'outrate']                                             # (W)
+                                
+            else:                                                                       # Read from the device
+                self._header = [ 'date',
+                                 'time',
+                                 'cell_temp',
+                                 'cell_pressure',
+                                 'co2']                
 
     def connect(self):
         try:
@@ -94,14 +162,15 @@ class Licor:
         
         return True
 
-    def read(self):                                                                     
+    def read(self, device):                                                                     
         raw = bs(self.con.readline(), 'lxml')                                           # Read a complete row input stream
 
-        raw = raw.li820.data                                                            # Define data structure
-        res = [ datetime.datetime.now().isoformat(';'),                                 # where will be stored data from the device
-                raw.celltemp.string,
-                raw.cellpres.string,
-                raw.co2.string, ]
+        if DEVICE is 820:
+            raw = raw.li820.data                                                        # Define data structure
+            res = [ datetime.datetime.now().isoformat(';'),                             # where will be stored data from the device
+                    raw.celltemp.string,
+                    raw.cellpres.string,
+                    raw.co2.string, ]
 
         if DEBUG:
             print ("\nNew Data Point")
@@ -110,14 +179,40 @@ class Licor:
                 
         return res
 
-    def config_W(self):                                                                 # Write a complete instruction row
-        conf = etree.Element(self._header[0])                                           # <li820>
-        cfgs = etree.SubElement(conf, self._header[9])                                  #   <cfg>
-        outr = etree.SubElement(cfgs, self._header[11])                                 #       <outrate>  --> 1
-        outr.text = "1"
-        conn = etree.SubElement(conf, self._header[1])                                  #   <rs232>
-        co2a = etree.SubElement(conn, self._header[5])                                  #       <co2abs>   --> False
-        co2a.text = "false"
+    def config_W(self, device):                                                         # Write a complete instruction row
+        if DEVICE is 820:
+            conf = etree.Element(self._header[0])                                       # <li820>
+            cfgs = etree.SubElement(conf, self._header[9])                              #   <cfg>
+            outr = etree.SubElement(cfgs, self._header[11])                             #       <outrate>  --> 1
+            outr.text = "1"
+            conn = etree.SubElement(conf, self._header[1])                              #   <rs232>
+            co2a = etree.SubElement(conn, self._header[5])                              #       <co2abs>   --> False
+            co2a.text = "false"
+            ivol = etree.SubElement(conn, self._header[6])                              #       <ivolt>    --> False
+            ivol.text = "false"
+            raws = etree.SubElement(conn, self._header[7])                              #       <raw>      --> False
+            raws.text = "false"
+
+        self.con.write(etree.tostring(conf, pretty_print = False))                      # Send command
+        print ("Input: " + etree.tostring(conf, pretty_print = False))                  # Licor answer (ACK true or false)
+        data_response = self.con.readline()
+        print ("Output: " + data_response)
+    
+    def config_R(self):                                                                 # Write a config request
+        info = etree.Element(self._header[0])                                           # Write the actual configuration
+        info.text = "?"                                                                 # <li820>?</li820>
+        
+        self.con.write(etree.tostring(info, pretty_print = False))
+        print ("Input: " + etree.tostring(info, pretty_print = False))
+        data_response = self.con.readline()
+        print ("Output: " + data_response)
+        
+    def data_loader(self):
+        a
+    
+    def __repr__(self):
+        return "Licor Model Li-{}".format(device_nr)
+
         ivol = etree.SubElement(conn, self._header[6])                                  #       <ivolt>    --> False
         ivol.text = "false"
         raws = etree.SubElement(conn, self._header[7])                                  #       <raw>      --> False
