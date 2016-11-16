d1ef6ea0065bb3733d74c2d7ecca270a037e0cea
OkTekk <mail@laurent-fournier.be>
2016-11-16 11:42:25 +0100

    Added devices switch

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 '''
     IO data from/to multiple sensors
     
@@ -9,12 +11,16 @@ import time, datetime
 import serial
 import argparse
 
-from bs4  import BeautifulSoup as bs
-from lxml import etree
+from bs4         import BeautifulSoup as bs
+from lxml        import etree
+from collections import Counter
 
-#import first_launch   as fl
-import file_manager   as fm
-import licor_8xx_6262 as li
+# External libraries
+import file_manager as fm
+from tools     import *
+from licor_6xx import Licor6xx
+from licor_7xx import Licor7xx
+from licor_8xx import Licor8xx
 
 #-------------------------------------------------------------
 #------------------ Open configurations ----------------------
@@ -30,7 +36,7 @@ parser.add_argument('-C', '--config',     type=bool, help='Configuration mode', 
 parser.add_argument('-d', '--debug',      type=bool, help='Debugging mode',        default=True,  choices=[True, False])
 parser.add_argument('-l', '--loops',      type=int,  help='Number of outputs',     default=5)
 parser.add_argument('-L', '--logging',    type=bool, help='Storing in .CSV files', default=True,  choices=[True, False])
-parser.add_argument('-m', '--model',      type=int,  help='Select device model',   default=820,   choices=[820, 840, 6262])
+parser.add_argument('-m', '--model',      type=int,  help='Select device model',   default=6262,  choices=[820, 840, 6262, 7000])
 args = parser.parse_args()
   
   ############
@@ -56,31 +62,38 @@ LOG_DIR    = 'logs/'
 
 #-------------------------------------------------------------
 #----- Better know what you are doing from this point --------
-#-------------------------------------------------------------
+#-------------------------------------------------------------    
+try:                                                                                    # Connect to device
+    if DEVICE == 820 or DEVICE == 840:
+        probe = Licor8xx(port=PORT, baud=BAUD, timeout=TIMEOUT, config=CONFIG, continuous=CONTINUOUS, debug=DEBUG, device=DEVICE, log=LOG, loops=LOOPS)
+        
+    elif DEVICE == 6262:
+        probe = Licor6xx(port=PORT, baud=BAUD, timeout=TIMEOUT, config=CONFIG, continuous=CONTINUOUS, debug=DEBUG, device=DEVICE, log=LOG, loops=LOOPS)
+
+    elif DEVICE == 7000:
+        probe = Licor7xx(port=PORT, baud=BAUD, timeout=TIMEOUT, config=CONFIG, continuous=CONTINUOUS, debug=DEBUG, device=DEVICE, log=LOG, loops=LOOPS)
+
+    probe.connect()
+    
+except Exception as e:
+    if DEBUG:
+        print ("ERROR: {}".format(e))
+    
+    sys.exit("Could not connect to the device")
 
   ###################
   # Writing routine #
   ###################
   
-if CONFIG:
-    try:                                                                                # Connect to device
-        licor = li.Licor(port=PORT,
-                         baud=BAUD,
-                         timeout=TIMEOUT,
-                         config=CONFIG,
-                         continuous=CONTINUOUS,
-                         debug=DEBUG,
-                         device=DEVICE,
-                         log=LOG,
-                         loops=LOOPS)
-        licor.connect()
-        licor.config_R()
-        #licor.config_W()
-        sys.exit("Configuration finished")
+if CONFIG:                                                                              # Configure the device if required
+    try:
+        #probe.config_R()
+        probe.config_W()
         
     except Exception as e:
-        print ("ERROR: {}".format(e))
-                
+        if DEBUG:
+            print ("ERROR: {}".format(e))
+        
         sys.exit("Could not connect to the device")
 
   ###################
@@ -92,52 +105,30 @@ filename = 'licor{}-data-{}.csv'.format(DEVICE, datetime.datetime.now())
 if LOG_DIR:                                                                             # If LOG_DIR is set, add it to filename
     filename = os.path.join(LOG_DIR, filename)
 
-try:                                                                                    # Connect to device
-    licor = li.Licor(port=PORT,
-                     baud=BAUD,
-                     timeout=TIMEOUT,
-                     config=CONFIG,
-                     continuous=CONTINUOUS,
-                     debug=DEBUG,
-                     device=DEVICE,
-                     log=LOG,
-                     loops=LOOPS)
-    licor.connect()
-    
-except Exception as e:
-    if DEBUG:
-        print ("ERROR: {}".format(e))
-        
-    sys.exit("Could not connect to the device")
-
-if LOG:                                                                                 # If logging enabled
+if LOG:                                                                                 # If logging is enabled
     with open(filename, 'w') as fp:
-        fp.write(';'.join(licor._header))                                               # Write headers
+        fp.write(';'.join(probe._header))                                               # Write headers
         fp.write('\n')
 
         while LOOPS:
             try:                
-                data = licor.read()                                                     # Read from device
-
+                data = probe.read()                                                     # Read from device
                 fp.write(';'.join(data))                                                # Write data
                 fp.write('\n')
                 
             except Exception as e:
-                if DEBUG:
-                    print ("ERROR: {}".format(e))
+                if DEBUG: print ("ERROR: {}".format(e))
                     
             time.sleep(FREQ)                                                            # Sleep for FREQ seconds
             
-            if not CONTINUOUS:
-                LOOPS -= 1
+            if not CONTINUOUS: LOOPS -= 1
                 
         fp.close()
         
-else:                                                                                   # If logging Disabled
+else:                                                                                   # If logging is Disabled
     while LOOPS:
-        data = licor.read()
+        data = probe.read()
         time.sleep(FREQ)
         
-        if not CONTINUOUS:
-            LOOPS -= 1
+        if not CONTINUOUS: LOOPS -= 1
             
