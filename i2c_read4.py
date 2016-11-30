6c0e2e30f03654c00c9b237f814f8be55878a425
OkTekk <mail@laurent-fournier.be>
2016-11-30 20:37:21 +0100

    I2C DAC ADC Tests

--- i2c_read4.py
+++ i2c_read4.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
 '''
-    ADC 8591 0x48
-    Dig 8574 0x27
+    
 '''
 
-from libs.ABE_ADCPi import ADCPi
-from libs.ABE_helpers import ABEHelpers
 import datetime, time
 import os, sys
 import smbus
@@ -16,20 +13,21 @@ DIG = 0x27
 b = smbus.SMBus(1)
 
 aTest = 0
-bTest = b'00000000'
+bTest = 0x00
 
-inc = 0
-i = 0
+i = 3
 j = 0
 
 while True:
-    aTest = bin(0 + inc)[2:].zfill(8)
+    aTest = bin(bTest + j)[2:].zfill(8)
     b.write_byte(ADC, int(aTest))
     reading = b.read_byte(ADC)
-    print ("ADC{} : Value {} - Read {}").format(inc, aTest, str(reading))
+    print ("ADC{} : Value {} - Read {}").format(i, aTest, str(reading))
    
-    inc += 1
-    if (inc > 3): inc = 0
+    i += 1
+    j += 1
+    if (i > 3): i = 0
+    if (j > 3): j = 0
     
     time.sleep(1)
 
